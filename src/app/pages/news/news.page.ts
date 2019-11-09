ea7b4126b75a4e8a3abe331b733020d85c3c1267
Rizvane <rizvane95140@gmail.com>
2019-11-09 20:54:52 +0100

    Barre de recherche fonctionnel avec titre de l'information

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm = '';
+
+
+  constructor(private newsService: NewsService) { }
 
   ngOnInit() {
   }
 
+  searchChanged() {
+     this.results = this.newsService.searchData(this.searchTerm);
+    // this.results.subscribe(res => {
+
+    // });
+  }
 }
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export enum SearchType {
+   all = '',
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  url = 'https://newsapi.org/v2/top-headlines?country=fr';
+  apiKey = '4e75f12cff6b493d90eaa616c7614eae';
+
+
+  constructor(private http: HttpClient) { }
+  searchData(title: string): Observable<any> {
+    return this.http.get(`${this.url}&q=${encodeURI(title)}&apiKey=${this.apiKey}`)
+    .pipe(
+      map(results => {
+        console.log('BRUTE: ', results);
+        return results['articles'];
+      })
+    );
+  }
+
+  getDetails(id) {
+    return this.http.get(`${this.url}?sources=$id&apiKey=${this.apiKey}`);
+  }
+
 }
+
